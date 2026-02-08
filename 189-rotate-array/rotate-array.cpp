2a0295e7a6f757cd577d95a3176331e3a3efa81d
KAMRAN <149958521+Kamran0018@users.noreply.github.com>
2026-02-08 15:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (91.12%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -6,10 +6,10 @@ public:
         int n = nums.size();
         k = k % n; 
         reverse(nums.begin(), nums.end()); 
-        // 7 6 5 4 3 2 1        
+              
         reverse(nums.begin(), nums.begin() + k);   
-        // 5 6 7 4 3 2 1
+        
         reverse(nums.begin() + k, nums.end()); 
-        // 5 6 7 1 2 3 4    
+           
     }
 };
