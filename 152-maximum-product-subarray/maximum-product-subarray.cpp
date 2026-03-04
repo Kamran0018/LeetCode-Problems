1098e367aac189600db9d9ed30a8213bf6a95583
KAMRAN <149958521+Kamran0018@users.noreply.github.com>
2026-03-04 23:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.05%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,8 +6,9 @@ public:
         int ans = nums[0];
         for(int i = 1; i < nums.size(); i++)
         {
-            if(nums[i] < 0)
+            if(nums[i] < 0){
                 swap(maxProd, minProd);
+                }
             maxProd = max(nums[i], maxProd * nums[i]);
             minProd = min(nums[i], minProd * nums[i]);
             ans = max(ans, maxProd);
